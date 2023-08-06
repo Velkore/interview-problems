5c6e19b118dc15b819aecc61bc521b7fb45923c3
Joshua Smith <jasmith0401@gmail.com>
2023-08-06 18:19:29 -0400

    Removed 'brute force', it was actually the same as using pointers

--- 3sum.js
+++ 3sum.js
@@ -10,17 +10,17 @@
 //*   [-1, -1, 2]
 //* ]
 
-//* Solution 1: Brute Force
+//* Solution 1: Pointers
 function threeSum(nums) {
-	//* Create a result array
+	//* Create a result array to store the triplets
 	const result = [];
 	//* Sort the input array in-place and ascending order
 	nums = nums.sort((a, b) => a - b);
 	//* Loop through the array
 	for (let i = 0; i < nums.length - 2; i++) {
-		//* Skip duplicates
+		//* Skip duplicates for i
 		if (i > 0 && nums[i] === nums[i - 1]) continue;
-
+		//* Create two pointers j and k
 		let j = i + 1;
 		let k = nums.length - 1;
 
@@ -87,47 +87,4 @@ console.log(threeSumHash([-1, 0, 1, 2, -1, -4])); //* O/P: [ [ -1, -1, 2 ], [ -1
 //? What is the space complexity of this solution? O(n) - We are creating a result array and a hash map
 //? What is the time complexity of this solution? O(n^2) - We are looping through the array and for each element we are looping through the rest of the array
 
-//* Solution 3: Using pointers
-// Refactor the above solution to use pointers
-function threeSumPointers(nums) {
-	//* Create a result array to store the triplets
-	const result = [];
-	//* Sort the input array in-place and ascending order
-	nums = nums.sort((a, b) => a - b);
-	//* Loop through the array starting from the first element
-	for (let i = 0; i < nums.length - 2; i++) {
-		//* Skip duplicates for i
-		if (i > 0 && nums[i] === nums[i - 1]) continue;
-		//* Create two pointers j and k
-		let j = i + 1;
-		let k = nums.length - 1;
-		//* Loop through the array starting from the next element after i
-		while (j < k) {
-			//* Calculate the sum of the current element and the elements at index j and k
-			let sum = nums[i] + nums[j] + nums[k];
-			//* If the sum is 0, push the triplet to the result array
-			if (sum === 0) {
-				result.push([nums[i], nums[j], nums[k]]);
-				j++;
-				k--;
-				//* Skip duplicates for j and k
-				while (j < k && nums[j] === nums[j - 1]) j++;
-				while (j < k && nums[k] === nums[k + 1]) k--;
-				//* If the sum is less than 0, increment j
-			} else if (sum < 0) {
-				j++;
-				//* If the sum is greater than 0, decrement k
-			} else {
-				k--;
-			}
-		}
-	}
-	//* Return the result array
-	return result;
-}
-
-console.log(threeSumPointers([-1, 0, 1, 2, -1, -4])); //* O/P: [ [ -1, -1, 2 ], [ -1, 0, 1 ] ]
-//? What is the space complexity of this solution? O(1) - We are not creating any additional space
-//? What is the time complexity of this solution? O(n log n) - We are looping through the array and for each element we are looping through the rest of the array
-
-//? Which solution is the best? Solution 3 is the best because it has the best space complexity and time complexity
+//? What is the difference between the two solutions? The first solution is more efficient in terms of space complexity but the second solution is more efficient in terms of time complexity
