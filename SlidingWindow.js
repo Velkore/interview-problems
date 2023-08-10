818b4e622bc1e8b101616aa7cb2ab95c4f6626b6
Joshua Smith <jasmith0401@gmail.com>
2023-08-09 21:27:53 -0400

    Update: Solved longest substring without repeating characters

--- SlidingWindow.js
+++ SlidingWindow.js
@@ -41,8 +41,39 @@ function subArraySum(nums, K) {
 	return totalSubArrays;
 }
 
-console.log(subArraySum([1, 2, 5, 1, 1, 3, 3], 7)); //* 3
-console.log(subArraySum([1, 2, 5, 1, 1, 3, 3], 6)); //* 4
+// console.log(subArraySum([1, 2, 5, 1, 1, 3, 3], 7)); //* 3
+// console.log(subArraySum([1, 2, 5, 1, 1, 3, 3], 6)); //* 2
 
 //? Time Complexity: O(n) - The algorithm runs in linear time since it iterates through the input array once.
 //? Space Complexity: O(1) - The extra space used by the algorithm is constant, regardless of the size of the input array.
+
+//* Using a sliding window algorithm, find the longest substring of input string without repeating characters
+
+function longestSubString(str) {
+	//* Initialize a set to store current substring
+	let tracker = new Set();
+	let windowStart = 0;
+	let windowEnd = 0;
+	let longest = 0;
+
+	//* Loop through the string
+	while (windowEnd < str.length && windowStart < str.length) {
+		//* If the current character is not in the set, add it to the set and increment the windowEnd
+		if (!tracker.has(str[windowEnd])) {
+			tracker.add(str[windowEnd]);
+			windowEnd++;
+			//* If the current character is in the set, delete the first character in the set and increment the windowStart
+		} else {
+			tracker.delete(str[windowStart]);
+			windowStart++;
+		}
+		//* Update the longest substring
+		longest = Math.max(longest, tracker.size);
+	}
+	//* Return the longest substring
+	return longest;
+}
+
+console.log(longestSubString("abcabcbb")); //* 3
+//? Time Complexity: O(n) - The algorithm runs in linear time since it iterates through the input string once.
+//? Space Complexity: O(n) - The extra space used by the algorithm is linear, since the size of the set is dependent on the size of the input string.
